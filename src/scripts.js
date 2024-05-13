5ede1913bf49e7f7acdc9aae3e6df3cae317ea9e
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-13 15:21:06 -0600

    adds getRandomUser function

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,24 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
+// pseudocode: use .find() to iterate over users array and find user element with an id that matches
+// the argument passed through the returnUserData function 
+
+function returnUserData(userId) {
+
+}
+
+
+
+function getRandomUser(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+exports = {
+    returnUserData, 
+    getRandomUser,
+}
+
 
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
