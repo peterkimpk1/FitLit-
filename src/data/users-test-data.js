58b7dcca0c4d827736f5adf958dbda16f85376d0
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-13 15:47:07 -0600

    adds js file with sample dataset for testing purposes

---
+++ src/data/users-test-data.js
@@ -0,0 +1,44 @@
+export default { usersTestData: [
+    {
+      "id": 1,
+      "name": "Peter Kim",
+      "address": "9484 Lucas Flat, West Kittymouth WA 67504",
+      "email": "Peter.Kim@gmail.com",
+      "strideLength": 4,
+      "dailyStepGoal": 7000,
+      "friends": [
+        5,
+        43,
+        46,
+        11
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Jordan Williamson",
+      "address": "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+      "email": "Jordan.Williamson@yahoo.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 9000,
+      "friends": [
+        13,
+        19,
+        3
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Willem Pol",
+      "address": "48010 Balistreri Harbor, Cleobury IN 43317",
+      "email": "Willem.Pol@gmail.com",
+      "strideLength": 2.7,
+      "dailyStepGoal": 3000,
+      "friends": [
+        31,
+        16,
+        15,
+        7
+      ]
+    },
+]
+}
