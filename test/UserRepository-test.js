dfc1a022ebef890435f326327e8ce03044fa8cfe
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-13 17:20:37 -0600

    adds should return user data based on their test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -7,21 +7,24 @@ describe('User Repository', () => {
   });
 });
 
-describe('Return User Data', () => {
-  it('should be a function', function () {
-    expect(returnUserData).to.be.a('function');
-  }); 
-  it.skip('should return user data based on their id', function () {
+describe('Return User Data', () => { 
+  it('should return user data based on their id', function () {
     const userId = 1;
-    const user1 = returnUserData(userId);
-
-    expect(user1.id).to.equal(1);
-    expect(user1.name).to.equal("Trystan Gorczany");
-    expect(user1.address).to.equal("9484 Lucas Flat, West Kittymouth WA 67504");
-    expect(user1.email).to.equal("Taurean_Pollich31@gmail.com");
-    expect(user1.strideLength).to.equal(4);
-    expect(user1.dailyStepGoal).to.equal(7000);
-    expect(user1.friends).to.deep.equal([5, 43, 46, 11]);
-
+    const user1 = returnUserData(userId)
+   
+    expect(user1).to.deep.equal({
+      "id": 1,
+      "name": "Peter Kim",
+      "address": "9484 Lucas Flat, West Kittymouth WA 67504",
+      "email": "Peter.Kim@gmail.com",
+      "strideLength": 4,
+      "dailyStepGoal": 7000,
+      "friends": [
+        5,
+        43,
+        46,
+        11
+      ]
+    });
   });
 }); 
