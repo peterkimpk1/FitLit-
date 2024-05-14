2d1da240c444cd97fef2bf5f53f6b4e5b149c8ec
Willem Pol <wpol.edu@gmail.com>
2024-05-14 03:58:55 -0600

    declared functions for goal/average step display

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,16 +1,9 @@
-//NOTE: Your DOM manipulation will occur in this file
+const displayUserGoal = () => {}
 
-//Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
-const exampleFunction1 = (person) => {
-  console.log(`oh hi there ${person}`)
-}
-
-const exampleFunction2 = (person) => {
-  console.log(`bye now ${person}`)
-}
+const displayAverageSteps = () => {}
 
 
-export {
-  exampleFunction1,
-  exampleFunction2,
+module.exports = {
+  displayUserGoal,
+  displayAverageSteps,
 }
