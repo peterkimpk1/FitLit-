4f992c740d894b72f48d24b0187312b33b2936c3
Willem Pol <wpol.edu@gmail.com>
2024-05-14 13:23:25 -0600

    changed module.exports to export

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,12 +2,13 @@ const userStepGoalContainer = document.querySelector('.user-step-goal')
 const averageStepContainer = document.querySelector('.average-goal-steps')
 
 const users = require('./data/users')
+const scripts = require('./')
 
 const displayUserGoal = () => {}
 const displayAverageSteps = () => {}
 
 
-module.exports = {
+export {
   displayUserGoal,
   displayAverageSteps,
 }
