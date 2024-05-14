27103100bb2724d47635a89614b5d8f06484659a
Peter Kim <peterkim.pk1@gmail.com>
2024-05-14 16:18:41 -0400

    Changed file name

---
+++ src/data/sample-hydration-test-data.js
@@ -0,0 +1,122 @@
+export default { hydrationSampleData: [
+    {
+      "userID": 1,
+      "date": "2023/03/24",
+      "numOunces": 10
+    },
+    {
+      "userID": 2,
+      "date": "2023/03/24",
+      "numOunces": 20
+    },
+    {
+      "userID": 3,
+      "date": "2023/03/24",
+      "numOunces": 25
+    },
+    {
+      "userID": 1,
+      "date": "2023/03/25",
+      "numOunces": 15
+    },
+    {
+      "userID": 2,
+      "date": "2023/03/25",
+      "numOunces": 5
+    },
+    {
+      "userID": 3,
+      "date": "2023/03/25",
+      "numOunces": 10
+    },
+    {
+    "userID": 1,
+    "date": "2023/03/26",
+    "numOunces": 20
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/26",
+    "numOunces": 25
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/26",
+    "numOunces": 15
+    },
+    {
+    "userID": 1,
+    "date": "2023/03/27",
+    "numOunces": 20
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/27",
+    "numOunces": 25
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/27",
+    "numOunces": 15
+    },
+    {
+    "userID": 1,
+    "date": "2023/03/28",
+    "numOunces": 20
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/28",
+    "numOunces": 25
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/28",
+    "numOunces": 15
+    },
+    {
+    "userID": 1,
+    "date": "2023/03/29",
+    "numOunces": 20
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/29",
+    "numOunces": 25
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/29",
+    "numOunces": 15
+    },
+    {
+    "userID": 1,
+    "date": "2023/03/30",
+    "numOunces": 20
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/30",
+    "numOunces": 25
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/30",
+    "numOunces": 15
+    },
+    {
+    "userID": 1,
+    "date": "2023/03/31",
+    "numOunces": 20
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/31",
+    "numOunces": 25
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/31",
+    "numOunces": 15
+    },
+]}
