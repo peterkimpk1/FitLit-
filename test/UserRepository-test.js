07eff734e5ea5b70559c7cfdde2dcac5c37f327b
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-13 20:54:36 -0600

    adds tests for returnUserData and returnAverageStepGoalAllUsers functions

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-const { returnUserData } = require('../src/scripts.js'); 
+const { returnUserData } = require('../src/scripts.js');
+const { returnAverageStepGoalAllUsers } = require('../src/scripts.js');
 
 describe('User Repository', () => {
   it('should run tests', function () {
@@ -20,11 +21,36 @@ describe('Return User Data', () => {
       "strideLength": 4,
       "dailyStepGoal": 7000,
       "friends": [
+        2,
+        3,
         5,
-        43,
-        46,
-        11
       ]
     });
   });
-}); 
+});
+
+describe('Return Avg Step Goal for All Users', function() {
+  it('should return the average step goal for all users', function () {
+    const userId = 1;
+    const user1 = returnUserData(userId);
+    
+    const userId2 = 2;
+    const user2 = returnUserData(userId2);
+    
+    const userId3 = 3;
+    const user3 = returnUserData(userId3);
+    
+    const userId4 = 4;
+    const user4 = returnUserData(userId4);
+  
+    const userId5 = 5; 
+    const user5 = returnUserData(userId5);
+  
+    const usersData = [user1, user2, user3, user4, user5];
+   
+    const avgStepGoalAllUsers = returnAverageStepGoalAllUsers(usersData);
+  
+        expect(avgStepGoalAllUsers).to.equal(6600);
+  });
+ 
+})
