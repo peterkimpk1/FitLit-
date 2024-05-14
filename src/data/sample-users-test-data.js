e4ca591b905a05bd5861fed0546265c7632d8b5b
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-13 20:53:23 -0600

    renames file to clarify it is a sample dataset of user data and revises the friends ids in each user.friends array to reflect the user ids present in the sample dataset

---
+++ src/data/sample-users-test-data.js
@@ -0,0 +1,69 @@
+export default { sampleUserDataset: [
+    {
+      "id": 1,
+      "name": "Peter Kim",
+      "address": "9484 Lucas Flat, West Kittymouth WA 67504",
+      "email": "Peter.Kim@gmail.com",
+      "strideLength": 4,
+      "dailyStepGoal": 7000,
+      "friends": [
+        2,
+        3,
+        5,
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Jordan Williamson",
+      "address": "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+      "email": "Jordan.Williamson@yahoo.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 9000,
+      "friends": [
+        1,
+        3,
+        4,
+        5
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Willem Pol",
+      "address": "48010 Balistreri Harbor, Cleobury IN 43317",
+      "email": "Willem.Pol@gmail.com",
+      "strideLength": 2.7,
+      "dailyStepGoal": 3000,
+      "friends": [
+        1,
+        2,
+        4,
+        5
+      ]
+    },
+    {
+      "id": 4,
+      "name": "Kim Ewing",
+      "address": "1378 Renner Island, Port Lincoln NE 06237-3602",
+      "email": "Kim66@yahoo.com",
+      "strideLength": 3.9,
+      "dailyStepGoal": 4000,
+        "friends": [
+          2,
+          3,
+          5
+        ]
+    },
+    {
+      "id": 5,
+      "name": "Seong Kang",
+      "address": "0770 Keeley Square, West Keyon SD 73400-6577",
+      "email": "Seong55@yahoo.com",
+      "strideLength": 3.3,
+      "dailyStepGoal": 10000,
+        "friends": [
+          2,
+          4,
+        ]
+      },
+]
+}
