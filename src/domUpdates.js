12d2845ec435cac9ba473f02559a69b3a2487723
Peter Kim <peterkim.pk1@gmail.com>
2024-05-15 21:29:00 -0400

    Created updateUserCard function to populate the user's info displayed in the welcome message

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,9 +1,13 @@
 import users from './data/users.js';
-import {getRandomUser} from '../src/userFunctions.js'
+import {getRandomUser, getUserData} from '../src/userFunctions.js'
 const allUsers = users.users
 const userMessageInfo = document.querySelector('.welcome-message');
 const userStepGoalContainer = document.querySelector('.user-step-goal')
 const averageStepContainer = document.querySelector('.average-goal-steps')
+const userIdAddressEmail = document.querySelector('.user-id-address-email')
+const userStrideLength = document.querySelector('.user-stride-length')
+const userDailySteps = document.querySelector('.user-daily-step-goal')
+
 
 window.addEventListener('load', () => {
   updateRandomUserMessage(allUsers);
@@ -15,7 +19,14 @@ const displayAverageSteps = () => {}
 
 function updateRandomUserMessage(users) {
   const randomUser = getRandomUser(users);
+  const user = getUserData(allUsers,randomUser.id)
+  updateUserCard(user)
   updateUserMessage(randomUser);
+}
+
+function updateUserCard(user) {
+  userIdAddressEmail.innerText = `ID: ${user.id}, Address: ${user.address}, Email: ${user.email}`
+  userStrideLength.innerText = `Stride Length: ${user.strideLength}`
 }
 
 const updateUserMessage = (users) => {  
