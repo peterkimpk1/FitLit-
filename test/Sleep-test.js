23c0174ca3930af523763e85e3485c57c99eefc9
Peter Kim <peterkim.pk1@gmail.com>
2024-05-17 18:31:50 -0400

    Created function/test to get a week's worth of user's sleep data

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import sleepData from "../src/data/sample-sleep-test-data.js"
 const sampleData = sleepData.sleepData
-const {getUserAverageHoursSlept, getUserAverageSleepQuality} = require('../src/sleep')
+const {getUserAverageHoursSlept, getUserAverageSleepQuality, getSleepHoursAndQualityForWeek} = require('../src/sleep')
 
 describe ('getUserAverageHoursSlept', () => {
     it ('should return a user/s average hours slept for all time', () => {
@@ -16,5 +16,22 @@ describe ('getUserAverageSleepQuality', () => {
         const userId = 1;
         const e = getUserAverageSleepQuality(sampleData, userId);
         expect(e).to.equal(4)
+    })
+})
+
+describe ('getSleepHoursAndQualityForWeek', () => {
+    it ('should return a user/s hours slept and sleep quality for a given week starting from the date', () => {
+        const userId = 1;
+        const date = '2023/03/25'
+        const e = getSleepHoursForWeek(sampleData, userId, date)
+        expect(e).to.equal([
+            {hoursSlept: 6.3, sleepQuality: 3.3},
+            {hoursSlept: 8.5, sleepQuality: 3.6},
+            {hoursSlept: 4.3, sleepQuality: 3.2},
+            {hoursSlept: 5.5, sleepQuality: 2.2},
+            {hoursSlept: 10.5, sleepQuality: 4.3},
+            {hoursSlept: 6.5, sleepQuality: 2.4},
+            {hoursSlept: 8.2, sleepQuality: 4.2}
+          ])
     })
 })
