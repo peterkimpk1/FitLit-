0bb69fd9d11604c881bb330f3a6ac81d94e09fb3
Peter Kim <peterkim.pk1@gmail.com>
2024-05-17 10:23:23 -0400

    Created function/test to get a user's average sleep quality for all time

--- src/sleep.js
+++ src/sleep.js
@@ -9,6 +9,18 @@ function getUserAverageHoursSlept(data, userId) {
     return Math.round(totalHours / dayCount)
 }
 
+function getUserAverageSleepQuality(data, userId) {
+    const totalSleepQuality = data.reduce((total,sleep) => {
+        if (sleep.userID === userId) {
+            total += sleep.sleepQuality
+        }
+        return total;
+    },0)
+    const dayCount = data.filter(sleep => sleep.userID === userId).length
+    return Math.round(totalSleepQuality / dayCount)
+}
+
 export {
-    getUserAverageHoursSlept
+    getUserAverageHoursSlept,
+    getUserAverageSleepQuality
 }
--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,12 +1,20 @@
 import { expect } from 'chai';
 import sleepData from "../src/data/sample-sleep-test-data.js"
 const sampleData = sleepData.sleepData
-const {getUserAverageHoursSlept} = require('../src/sleep')
+const {getUserAverageHoursSlept, getUserAverageSleepQuality} = require('../src/sleep')
 
 describe ('getUserAverageHoursSlept', () => {
     it ('should return a user/s average hours slept for all time', () => {
         const userId = 1;
         const e = getUserAverageHoursSlept(sampleData,userId);
         expect(e).to.equal(7)
+    })
+})
+
+describe ('getUserAverageSleepQuality', () => {
+    it ('should return a user/s average sleep quality for all time', () => {
+        const userId = 1;
+        const e = getUserAverageSleepQuality(sampleData, userId);
+        expect(e).to.equal(4)
     })
 })
