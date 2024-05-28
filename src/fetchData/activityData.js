e05574cf68c92465293d6b045b508376e02f7000
Peter Kim <peterkim.pk1@gmail.com>
2024-05-28 18:59:11 -0400

    Fixed fetch calls to take dynamic input. Deleted fetchData files

--- src/fetchData/activityData.js
+++
@@ -1,8 +0,0 @@
-const fetchActivities = () => {
-    const newFetch = fetch('https://fitlit-api.herokuapp.com/api/v1/activity').then(response => response.json())
-    return newFetch
-}
-fetchActivities()
-export {
-    fetchActivities
-}
