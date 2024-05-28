8e2ff42b8d3d9f7093aef8a8ca188e32bc551fe9
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-28 14:54:45 -0600

    adds refactored fetchData function to apiCalls file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,12 @@
-// Your fetch requests will live here!
-
-
-console.log('I will be a fetch request!')
+function fetchData(dataType) {
+    const root = 'https://fitlit-api.herokuapp.com/api/v1/'
+    const url = `${root}${dataType}`
+    const promise = fetch(url)
+                    .then(response => response.json());
+    return promise
+    }
+    
+    export {
+        fetchData
+    }
 
